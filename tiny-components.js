52d01b6199147597f542043e39331653dac38b2c
Yannick Schössow <102901459+YanSchw@users.noreply.github.com>
2024-05-23 21:44:23 +0200

    Added Basic Components

--- tiny-components.js
+++ tiny-components.js
@@ -6,14 +6,47 @@ if (!window._libTinyComponentLoaded) {
 }
 
 const internal = {
-    initializeAllComponents() {
-        console.log('initializeAllComponents()');
-    },
-    
+    globalIdCounter: 1,
+    componentMap: new Map(),
+    domToObjMap: new Map()
 };
 
+function constructComponentInnerHTML(nodeDOM, func) {
+    func(nodeDOM, internal.domToObjMap.get(nodeDOM));
+}
+function createComponentObject(nodeDOM) {
+    if (!internal.domToObjMap.has(nodeDOM)) {
+        let obj = {};
+        obj.tinyid = internal.globalIdCounter++;
+        internal.domToObjMap.set(nodeDOM, obj);
+        nodeDOM.classList.add(`tiny-id-${obj.tinyid}`);
+    }
+}
 
+function initializeComponent(nodeDOM, func) {
+    if (!nodeDOM.classList.contains('tiny-component')) {
+        nodeDOM.classList.add('tiny-component');
+        createComponentObject(nodeDOM);
+        constructComponentInnerHTML(nodeDOM, func);
+    }
+}
+
+function initializeAllComponents() {
+    for (let comp of internal.componentMap) {
+        let nodeList = document.querySelectorAll(comp[0]);
+        for (let nodeDOM of nodeList) {
+            initializeComponent(nodeDOM, comp[1]);
+        }
+    }
+}
 
 function component(tag, func) {
-    console.log(tag);
+    internal.componentMap.set(tag, func);
 }
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    initializeAllComponents();
+});
+const observer = new MutationObserver((mutations) => {
+    initializeAllComponents();
+});
