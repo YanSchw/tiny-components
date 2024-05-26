447ab04bb1b3cc00371f7238f6c84f5e32f9b325
Yannick Schössow <102901459+YanSchw@users.noreply.github.com>
2024-05-26 15:59:35 +0200

    Added else component

--- components/if/if.js
+++ components/if/if.js
@@ -10,4 +10,20 @@ component('if', (nodeDOM, obj) => {
     if (obj.result) {
         nodeDOM.innerHTML = obj.innerHTML;
     }
+
+    let elseComponent = select(`if.tiny-id-${obj.tinyid} + else`);
+    if (elseComponent != null && elseComponent != undefined) {
+        elseComponent.ifResult = obj.result;
+        internal.domToObjMap.get(elseComponent).redraw();
+    }
+});
+
+component('else', (nodeDOM, obj) => {
+    if (nodeDOM.ifResult == undefined) {
+        return;
+    }
+    
+    if (!nodeDOM.ifResult) {
+        nodeDOM.innerHTML = obj.innerHTML;
+    }
 });
